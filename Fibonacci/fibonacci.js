a1e8e4b17c5c4cbe614424f782d9565f14adaaee
Emmanuel <emmysoetan@gmail.com>
2022-10-21 05:46:43 +0100

    fibonnaci series conculsion

--- Fibonacci/fibonacci.js
+++ Fibonacci/fibonacci.js
@@ -10,6 +10,8 @@
  For example, given n = 2, the sequence is [0, 1]
  given n = 3, the sequence is [ 0,1,1]
  given n = 4 , the sequence is [0,1,1,2]
+ given n = 5 , the sequence is [0,1,1,2,3]
+ given n = 6 , the sequence is [0,1,1,2,3,5,]
  given n = 7, the sequence is [0,1,1,2,3,5,8]
 
 
@@ -21,7 +23,7 @@
   2. pass in the parameter n
   3. create and empty array and assign the first two elements which are 0 and 1
   4. use a for loop to interate from the index position of 2
-  5. The sequence then becomes the index position minus 1 added the the index postion minus 2
+  5. The sequence then becomes the index position minus 1 added to the index postion minus 2
 */
 
 function fibonnaci(n){
@@ -35,3 +37,6 @@ function fibonnaci(n){
 
 fibonnaci(7)
 fibonnaci (3)
+fibonnaci(5)
+
+/* The time complexity for finding the fibonnaci series of a given number n is linear because as the input size increases the time required also increases along side */
